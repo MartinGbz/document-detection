3e8ce3f1ff40f1ed0c284e98b9ea22591728cf02
MartinGbz <martin.grabarz@outlook.fr>
2023-01-26 12:42:59 +0100

    code cleaning

--- js/processing-live.js
+++ js/processing-live.js
@@ -43,7 +43,7 @@ function globalProcess(src) {
     // hierarchy = new cv.Mat();
     cv.findContours(src, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
 
-    // // show all contours found
+    // show all contours found
     // drawAllContours();
 
     // Create convex hulls from different contours
@@ -82,19 +82,17 @@ function filtersProcess(src) {
 
     //resize img
     // cv.resize(src, srcResized, dsize, 0, 0, cv.INTER_AREA);
-    //cv.imshow('canvasOutput2', srcResized);
+    // cv.imshow('canvasOutput2', srcResized);
 
     // RGB to BGR
     cv.cvtColor(src, img, cv.COLOR_RGB2BGR)
-    // cv.imshow('canvasOutput3', img);
 
     img2 = img.clone();
-    img3 = img.clone();
+    // img3 = img.clone();
     // img4 = img.clone();
 
     // BGR to GRAY levels
     cv.cvtColor(img, img, cv.COLOR_BGR2GRAY)
-    // cv.imshow('canvasOutput4', gray);
 
     // cv.cvtColor(src, img, cv.COLOR_RGBA2GRAY);
 
@@ -103,24 +101,16 @@ function filtersProcess(src) {
     // cv.imshow('canvasOutput5', bilateral);
 
 
-    // let ksize = new cv.Size(17, 17);
-    // let ksize = new cv.Size(29, 29);
-    // let ksize = new cv.Size(11, 11);
+    // Gaussian blur
     cv.GaussianBlur(img, img, ksize, 0, 0, cv.BORDER_DEFAULT)
 
     // ----------------------------------------
 
     // Equalize histoigram
     cv.equalizeHist(img, img)
-    //cv.imshow('canvasOutput6', eq);
 
     // Canny filter
-    // cv.Canny(gray, edged, 200, 250)
     cv.Canny(img, img, 75, 200)
-    //cv.imshow('canvasOutput7', edged);
-
-    // cv.HoughLines(gray, linesTest, 1, Math.PI/180,15)
-    // cv.imshow('canvasOutput51', linesTest);  
 
     return img
 }
@@ -191,7 +181,7 @@ function findBiggestContourAndHullRect() {
     boundRect = new cv.RectVector();
 
     // explore contours and draw all of them on the img
-    // finds the bigest contour
+    // finds the biggest contour
     for (let i = 0; i < contours.size(); ++i) {
         let red = Math.floor(Math.random() * (Math.floor(255) - Math.ceil(0) + 1) + Math.ceil(0));
         let green = Math.floor(Math.random() * (Math.floor(255) - Math.ceil(0) + 1) + Math.ceil(0));
@@ -222,14 +212,7 @@ function findBiggestContourAndHullRect() {
         else {
             contourSelectedRect = rect2;
         }
-
-        let topLeft = new cv.Point(boundRect.get(i).x, boundRect.get(i).y);
-        let bottomRight = new cv.Point(boundRect.get(i).x + boundRect.get(i).width, boundRect.get(i).y + boundRect.get(i).height);
-        cv.rectangle(img3, topLeft, bottomRight, color, 2);
     }
-
-    cv.cvtColor(img3, img3, cv.COLOR_BGR2RGB)
-    // cv.imshow('canvasOutput9', img3);
 
     // create an empty MatVector and put the biggest contour in it
     let green = new cv.Scalar(0,255,0);
@@ -239,50 +222,15 @@ function findBiggestContourAndHullRect() {
     // Draw the bigest contour hulled on the idcard
     let hull2 = new cv.MatVector();
     biggestContourHulled = new cv.Mat()
-    // biggestContourHulled2 = new cv.Mat()
-
-    let testtest = new cv.MatVector();
-    testtest.push_back(contourSelected);
-    // cv.drawContours(img8, testtest, 0, green, 5, cv.LINE_8, hierarchy, 0);
-    // cv.cvtColor(img8, img8, cv.COLOR_BGR2RGB, 0);
-    // cv.imshow('canvasOutput57', img8);
     
-    // Ici le fait de hulled le contour me permet dans findcorner de le faire passé en tant que rectangle
-    // alors que le contour en lui même ne serait pas passé
-    // le mieux serait d'utiliser la fonction boudingRect (utilisé dans findcorner2) mais pour l'instant l'homographie findcroner2 ne marche pas.
-    // mais une fois ça résolut c'est good.
     cv.convexHull(contourSelected, biggestContourHulled, true, true);
-
-
-    // let a = new cv.Mat(biggestContourHulled)
-    // let minRect = cv.minAreaRect(a);
-    // console.log('minRect:', minRect)
-    // let topLeft2 = new cv.Point(minRect.center.x, minRect.center.y);
-    // let bottomRight2 = new cv.Point(minRect.center.x + minRect.size.width, minRect.center.y + minRect.size.height);
-    // cv.rectangle(img9, topLeft2, bottomRight2, green, 2);
-    // cv.imshow('canvasOutput13', img9);
 
     cv.approxPolyDP(biggestContourHulled, biggestContourHulled, 100, true);
     
-    // let rect2 = cv.boundingRect(biggestContourHulled);
-    // biggestContourHulled2 = img5.roi(rect2);
-    
     hull2.push_back(biggestContourHulled);
-    
-    let hull3 = new cv.MatVector();
-    hull3.push_back(biggestContourHulled);
-    // cv.drawContours(img6, hull3, 0, green, 5, cv.LINE_8, hierarchy, 0);
-    // cv.cvtColor(img6, img6, cv.COLOR_BGR2RGB, 0);
-    // cv.imshow('canvasOutput55', img6);
-
-    let topLeft = new cv.Point(contourSelectedRect.x, contourSelectedRect.y);
-    let bottomRight = new cv.Point(contourSelectedRect.x + contourSelectedRect.width, contourSelectedRect.y + contourSelectedRect.height);
-    // cv.rectangle(img7, topLeft, bottomRight, green, 2);
-    // cv.imshow('canvasOutput56', img7);
 
     cv.drawContours(img2, hull2, 0, green, 5, cv.LINE_8, hierarchy, 0);
     cv.cvtColor(img2, img2, cv.COLOR_BGR2RGB, 0);
-    // cv.imshow('canvasOutput10', img5);
 }
 
 // function findCorners(){
--- js/video.js
+++ js/video.js
@@ -17,32 +17,16 @@ function startVideo() {
     
         function processVideo() {
           try {
-            // const memUsedBefore = window.performance.memory['usedJSHeapSize']; //Used memory before executing processVideo (in bytes)
-            // if (!streaming) {
-            //   // clean and stop.
-            //   src.delete();
-            //   dst.delete();
-            //   return;
-            // }
             let begin = Date.now();
-            // start processing.
             cap.read(src);
             dst = globalProcess(src);
-            // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
             cv.imshow("videoOutput", dst);
-            // document.getElementById("canvasOutput").getContext("2d", { willReadFrequently: true })
-            // schedule the next one.
             let delay = 1000 / FPS - (Date.now() - begin);
-            //Subtract memUsedBefore from the current allocated heap size
-            // const additionalMemUsed = window.performance.memory['usedJSHeapSize'] - memUsedBefore;
-            // console.log('additionalMemUsed:', additionalMemUsed)
             setTimeout(processVideo, delay);
           } catch (err) {
             console.error(err);
           }
         }
-    
-        // schedule the first one.
         setTimeout(processVideo, 0);
       })
       .catch(function(err) {
